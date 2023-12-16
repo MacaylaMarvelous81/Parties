f3f8535221fc1bab17944a3f6a74989d8f1f929e
Jomar Milan <jomarm@jomarm.tech>
2023-12-15 23:05:42 -0800

    Center Start screen
    Put the start screen content in the center of the screen

--- src/client/screens/start.tsx
+++ src/client/screens/start.tsx
@@ -9,35 +9,37 @@ export default function StartScreen() {
 
 	return (
 		<frame Size={ UDim2.fromScale(1, 1) } BackgroundColor3={ color.jet }>
-			<uilistlayout
-				FillDirection={ Enum.FillDirection.Vertical }
-				Padding={ new UDim(0, rem(1.8125)) } />
-			<uipadding
-				PaddingTop={ new UDim(0, rem(1.375)) }
-				PaddingBottom={ new UDim(0, rem(1.375)) }
-				PaddingLeft={ new UDim(0, rem(3.75)) }
-				PaddingRight={ new UDim(0, rem(3.75)) } />
-			<textlabel
-				Size={ UDim2.fromOffset(rem(13.625), rem(4.25)) }
-				BackgroundTransparency={ 1 }
-				TextColor3={ color.white }
-				FontFace={ font.exbold }
-				TextSize={ rem(3.5) }
-				Text="Parties" />
-			<LongButton
-				icon={ assets.ui.icons["plus-lg"] }
-				title="Create a Party"
-				description="Your friends will be able to join your party. You can also share the invite code or link to invite others."
-				textColor={ color.white }
-				image={ assets.ui.button.long.inviting }
-				badge={ false } />
-			<LongButton
-				icon={ assets.ui.icons["box-arrow-in-right"] }
-				title="Join a Party"
-				description="You can join a friend's party or find a party by its invite code."
-				textColor={ color.black }
-				image={ assets.ui.button.long.placeholder }
-				badge={ false } />
+			<frame AutomaticSize={ Enum.AutomaticSize.XY } AnchorPoint={ new Vector2(0.5, 0.5) } Position={ UDim2.fromScale(0.5, 0.5) } BackgroundTransparency={ 1 }>
+				<uilistlayout
+					FillDirection={ Enum.FillDirection.Vertical }
+					Padding={ new UDim(0, rem(1.8125)) } />
+				<uipadding
+					PaddingTop={ new UDim(0, rem(1.375)) }
+					PaddingBottom={ new UDim(0, rem(1.375)) }
+					PaddingLeft={ new UDim(0, rem(3.75)) }
+					PaddingRight={ new UDim(0, rem(3.75)) } />
+				<textlabel
+					Size={ UDim2.fromOffset(rem(13.625), rem(4.25)) }
+					BackgroundTransparency={ 1 }
+					TextColor3={ color.white }
+					FontFace={ font.exbold }
+					TextSize={ rem(3.5) }
+					Text="Parties" />
+				<LongButton
+					icon={ assets.ui.icons["plus-lg"] }
+					title="Create a Party"
+					description="Your friends will be able to join your party. You can also share the invite code or link to invite others."
+					textColor={ color.white }
+					image={ assets.ui.button.long.inviting }
+					badge={ false } />
+				<LongButton
+					icon={ assets.ui.icons["box-arrow-in-right"] }
+					title="Join a Party"
+					description="You can join a friend's party or find a party by its invite code."
+					textColor={ color.black }
+					image={ assets.ui.button.long.placeholder }
+					badge={ false } />
+			</frame>
 		</frame>
 	);
 }
